568162bd5102b0af907127021daab68688fcfdfc
mdadilraza <mdadilraza15@gmail.com>
2024-11-25 12:27:07 +0530

    changed crons salary increment

--- employee-service/src/main/java/com/example/repository/EmployeeRepository.java
+++ employee-service/src/main/java/com/example/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.example.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Employee findByPhoneNumber(String phoneNumber);
 }
--- employee-service/src/main/java/com/example/service/SalaryServiceImpl.java
+++ employee-service/src/main/java/com/example/service/SalaryServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.entity.Salary;
 import com.example.repository.SalaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,15 +17,18 @@ public class SalaryServiceImpl implements SalaryService {
 
     @Autowired
     private SalaryRepository salaryRepository;
-
+    @Value("${percentageHike}")
+    private int percentageHike;
+    
     @Override
-    @Scheduled(cron = "0 37 16 * * ?")
+    @Scheduled(cron = "0 0 0 1 1 ?") //exact 12 am in new year salary will increment
     public List<Salary> incrementSalaryAll() {
         List<Salary> salaryList = salaryRepository.findAll();
         List<Salary> newSalaryList = new ArrayList<>();
 
+
         for (Salary salary : salaryList) {
-            double afterIncrement = salary.getSalary() + (salary.getSalary() * 10) / 100;
+            double afterIncrement = salary.getSalary() + (salary.getSalary() * percentageHike) / 100;
             salary.setSalary(afterIncrement);
             newSalaryList.add(salary);
         }
@@ -35,7 +39,7 @@ public class SalaryServiceImpl implements SalaryService {
     @Override
     public Salary incrementSalary(Integer id, Salary salary) {
         Salary empSalary = salaryRepository.findByEmployeeId(id);
-        double afterIncrement = empSalary.getSalary() + (empSalary.getSalary() * 10) / 100;
+        double afterIncrement = empSalary.getSalary() + (empSalary.getSalary() * percentageHike) / 100;
         empSalary.setSalary(afterIncrement);
         return salaryRepository.save(empSalary);
     }
