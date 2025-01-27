3b0a7fdcf1923cd892efd1cdac3f8e7e081d5da7
mdadilraza <mdadilraza15@gmail.com>
2025-01-27 11:34:23 +0530

    removed unused import

--- stock-service/src/main/java/com/example/stock_service/entity/Stock.java
+++ stock-service/src/main/java/com/example/stock_service/entity/Stock.java
@@ -1,13 +1,9 @@
 package com.example.stock_service.entity;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- stock-service/src/main/java/com/example/stock_service/util/ApiResponse.java
+++ stock-service/src/main/java/com/example/stock_service/util/ApiResponse.java
@@ -3,7 +3,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Data
 @AllArgsConstructor
